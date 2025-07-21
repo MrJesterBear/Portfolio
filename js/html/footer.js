e06359def2e410a38638de3ddd113f92788129cc
Saul Maylin <96966989+MrJesterBear@users.noreply.github.com>
2025-07-22 00:21:12 +0100

    Create Base Structure of the site.
    - Adapted an index page used from various projects.
    
    - Turned the footer into a dynamic js file. Made it so  the year is always current for the copy right notice too.
    
    - added some icons and a portrait for the top of the page.

---
+++ js/html/footer.js
@@ -0,0 +1,17 @@
+export function setFooter() {
+  // get footer element
+  const footer = document.getElementsByClassName("footer")[0];
+
+    // Get current year for copyright notice
+    const year = new Date().getFullYear();
+
+    // set content.
+    let footerHTML = 
+    "<p class='text-white'>©" + year + " Saul Maylin. All rights reserved.</p>" +
+        // Github
+        "<a class='text-white' href='https://github.com/MrJesterBear'><img src='./assets/logos/github.png' alt='Github Logo' width='30' height='30'></a>" +
+        // LinkedIn
+        "<a class='text-white' href='https://www.linkedin.com/in/saulmaylin/'><img src='./assets/logos/linkedin.png' alt='LinkedIn Logo' width='30' height='30'></a>";
+
+  footer.innerHTML = footerHTML;
+}
---
+++ js/html/nav.js
@@ -0,0 +1,47 @@
+// Saul Maylin
+// 18/06/2025
+// v1
+// Javascript HTML.
+
+// Navbar Function.
+// import { checkCookie, setCookie } from "../util/cookies.js";
+
+export function setNav() {
+  // get nav element
+  const nav = document.getElementsByClassName("nav")[0];
+
+  // Sets the nav bar! Taken from Bootstrap and reformatted to fit the site.
+  // https://getbootstrap.com/docs/5.3/components/navbar/
+
+  let navHTML =
+    // Nav Container and Hamburger Menu
+    '<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">' +
+    '<span class="navbar-toggler-icon"></span>' +
+    "</button>" +
+    '<div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">' +
+    '<ul class="navbar-nav">' +
+
+    // About
+    '<li class="nav-item px-5">' +
+    '<a class="nav-link border" href="#about">About</a>' +
+    "</li>" +
+
+    // Education
+    '<li class="nav-item px-5">' +
+    '<a class="nav-link border" href="#education">Education</a>' +
+    "</li>" +
+
+    // Projects
+    '<li class="nav-item px-5">' +
+    '<a class="nav-link border" href="#projects">Projects</a>' +
+    "</li>";
+
+    // Extra Link
+    // '<li class="nav-item px-5">' +
+    // '<a class="nav-link border" href="linkhere">wordhere</a>' +
+    // "</li>";
+
+  // close up the nav and make inner html.
+  navHTML += "</ul> </div>";
+  nav.innerHTML = navHTML;
+}
